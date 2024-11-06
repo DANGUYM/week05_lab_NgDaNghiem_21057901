86fa438094b1970076d0ed4928204aaa9f5cdea4
Tên mới của bạn <email_moi@example.com>
2024-11-07 04:10:43 +0700

    commit buoi thuc hanh

---
+++ src/test/java/vn/edu/iuh/fit/week05_lab_ngdanghiem_21057901/Week05LabNgDaNghiem21057901ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.week05_lab_ngdanghiem_21057901;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week05LabNgDaNghiem21057901ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
