42b7f52aeec7bb478a10463a42462231e998dff7
danghiemnguyen@gmail.com <danghiemnguyen@gmail.com>
2024-12-15 03:36:20 +0700

    fix repositories

--- src/main/java/vn/edu/iuh/fit/backend/repositories/CandidateSkillRepository.java
+++ src/main/java/vn/edu/iuh/fit/backend/repositories/CandidateSkillRepository.java
@@ -1,8 +1,15 @@
 package vn.edu.iuh.fit.backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import vn.edu.iuh.fit.backend.models.CandidateSkill;
-import vn.edu.iuh.fit.backend.models.Skill;
+import vn.edu.iuh.fit.backend.models.CandidateSkillId;
 
-public interface CandidateSkillRepository extends JpaRepository<CandidateSkill, Skill> {
+import java.util.List;
+
+@Repository
+public interface CandidateSkillRepository extends JpaRepository<CandidateSkill, CandidateSkillId> {
+    @Query("SELECT cs FROM CandidateSkill cs JOIN FETCH cs.skill WHERE cs.can.id = :canId")
+    List<CandidateSkill> findByCandidateIdWithSkill(Long canId);
 }
--- src/main/java/vn/edu/iuh/fit/backend/repositories/JobSkillRepository.java
+++ src/main/java/vn/edu/iuh/fit/backend/repositories/JobSkillRepository.java
@@ -1,8 +1,13 @@
 package vn.edu.iuh.fit.backend.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import vn.edu.iuh.fit.backend.models.JobSkill;
-import vn.edu.iuh.fit.backend.models.Skill;
+import vn.edu.iuh.fit.backend.models.JobSkillId;
 
-public interface JobSkillRepository extends JpaRepository<JobSkill, Skill> {
+import java.util.List;
+
+@Repository
+public interface JobSkillRepository extends JpaRepository<JobSkill, JobSkillId> {
+    List<JobSkill>  getJobSkillsByJobId(Long jobId);
 }
